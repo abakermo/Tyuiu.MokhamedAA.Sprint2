f685d180c8263c55cd04f1bf4ce8f19109b0a361
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-11-28 20:39:04 +0300

    Task2

---
+++ Tyuiu.MokhamedAA.Sprint2.Task2.V12/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.MokhamedAA.Sprint2.Task2.V12.Lib;
+namespace Tyuiu.MokhamedAA.Sprint2.Task2.V12
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("***********************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                *");
+        Console.WriteLine("***********************************************************************************");
+
+        Console.WriteLine(" Введите значение Х = ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(" Введите значение Y = ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        DataService ds = new DataService();
+        bool res = ds.CheckDotInShadedArea(x, y);
+
+   
+        Console.WriteLine("***********************************************************************************");
+        Console.WriteLine("*РЕЗУЛЬТАТ:                                                                       *");
+        Console.WriteLine("***********************************************************************************");
+
+        if (res)
+        {
+            Console.WriteLine(" Точка находится в заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine(" Точка не находится в заштрихованной области");
+        }
+
+        Console.ReadKey();
+
+    }
+}
