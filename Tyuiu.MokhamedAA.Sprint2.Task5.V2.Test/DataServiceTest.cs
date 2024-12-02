68f71225edd55a63cc68146a2824ea278b2f4b85
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-02 19:15:23 +0300

    Task5

---
+++ Tyuiu.MokhamedAA.Sprint2.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.MokhamedAA.Sprint2.Task5.V2.Lib;
+namespace Tyuiu.MokhamedAA.Sprint2.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MokhamedAA.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.MokhamedAA.Sprint2.Task5.V2.Lib;
+namespace Tyuiu.MokhamedAA.Sprint2.Task5.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите номер месяца: ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+            string res;
+            if ((numMouth < 1) || (numMouth > 12)) res = "Введено неверное значение!";
+            else res = "Это сезон года: " + ds.FindMonthSeason(numMouth);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
