92b1375a5bc02236081c51f7c882f5faab90c472
Tyuiu.RubankoGV@gmail.com <Tyuiu.RubankoGV@gmail.com>
2024-12-02 19:39:12 +0300

    Task6

---
+++ Tyuiu.MokhamedAA.Sprint2.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.MokhamedAA.Sprint2.Task6.V11.Lib;
+namespace Tyuiu.MokhamedAA.Sprint2.Task6.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int g = 2004;
+            int m = 4;
+            int n = 1;
+            DataService ds = new DataService();
+            Assert.AreEqual("31.03.2004", ds.FindDateOfNextDay(g, m, n));
+        }
+    }
+}
---
+++ Tyuiu.MokhamedAA.Sprint2.Task6.V11/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.MokhamedAA.Sprint2.Task6.V11.Lib;
+namespace Tyuiu.MokhamedAA.Sprint2.Task6.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int g, m, n;
+            DataService ds = new DataService();
+            
+            Console.WriteLine("**********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                               *");
+            Console.WriteLine("**********************************************************************************");
+            Console.WriteLine("* Введите G:                                                                     *");
+            g = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("* Введите M:                                                                     *");
+            m = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("* Введите N:                                                                     *");
+            n = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("**********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                     *");
+            Console.WriteLine("**********************************************************************************");
+            Console.WriteLine(ds.FindDateOfNextDay(g, m, n));
+
+            Console.ReadLine();
+        }
+    }
+}
